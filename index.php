4f1ae13fb1c45fc15601ae823d54840ebfb2c758
Uwe Kohnle <kohnle@internetlehrer-gmbh.de>
2023-08-21 07:07:40 +0000

    before commit for ILIAS 8

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-ini_set('display_errors', 1);
-ini_set('error_reporting', 5);
-
-require_once(__DIR__ . '/classes/class.ilMultiVcInit4Guestlink.php');
-
-$join = JoinMeetingByGuestLink::init();
-echo $join;
